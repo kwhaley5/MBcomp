286ca6ceff66729f57cdbe8db1777daceccd3510
Kyle Whaley <kylewhaley5@gmail.com>
2022-08-03 13:27:17 -0400

    Global Controls functionality
    Added the positioning and linked the sliders to Xovers and gains

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -13,6 +13,81 @@
 
 //==============================================================================
 
+struct LookAndFeel : juce::LookAndFeel_V4 {
+    void drawRotarySlider(juce::Graphics&, int x, int y, int width, int height,
+        float sliderPosProportional,
+        float rotaryStartAngle,
+        float rotaryEndAngle,
+        juce::Slider&) override;
+     
+    void drawToggleButton(juce::Graphics& g,
+        juce::ToggleButton& toggleButton,
+        bool shouldDrawButtonAsHighlighted,
+        bool shouldDrawButtonasDown) override;
+
+};
+
+struct RotarySliderWithLabels : juce::Slider {
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+        juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox),
+        param(&rap),
+        suffix(unitSuffix)
+    {
+        setLookAndFeel(&lnf);
+    }
+
+    ~RotarySliderWithLabels() {
+        setLookAndFeel(nullptr);
+    }
+
+    struct LabelPos {
+        float pos;
+        juce::String label;
+
+    };
+
+    juce::Array<LabelPos> labels;
+
+    void paint(juce::Graphics& g) override;
+    juce::Rectangle<int>  getSliderBounds() const;
+    int getTextHeight() const { return 14; };
+    juce::String getDisplayString() const;
+
+private:
+    LookAndFeel lnf;
+
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
+
+};
+
+struct RotarySlider : juce::Slider
+{
+    RotarySlider() :
+    juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                 juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {}
+};
+
+template < //template agrument deduction on cpp reference to understand what the heck this is
+    typename Attachment,
+    typename APVTS,
+    typename Params,
+    typename ParamName,
+    typename SliderType
+>
+
+void makeAttachment(std::unique_ptr<Attachment>& attachment,
+                    APVTS& apvts,
+                    const Params& params,
+                    const ParamName& name,
+                    SliderType& slider)
+{
+    attachment = std::make_unique<Attachment>(apvts,
+                                              params.at(name),
+                                              slider);
+}
+
 struct Placeholder : juce::Component
 {
 
@@ -24,6 +99,26 @@ struct Placeholder : juce::Component
     }
 
     juce::Colour customColor;
+};
+
+    
+
+struct GlobalControls : juce::Component
+{
+    GlobalControls(juce::AudioProcessorValueTreeState& apvts);
+
+    void paint(juce::Graphics& g) override;
+
+    void resized() override;
+    
+private:
+    RotarySlider inGainSlider, lowMidXoverSlider, midHighXoverSlider, outGainSlider;
+
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    std::unique_ptr<Attachment> lowMidXoverSliderAttachment,
+                                midHighXoverSliderAttachment,
+                                inGainSliderAttachment,
+                                outGainSliderAttachment;
 };
 
 /**
@@ -43,7 +138,8 @@ private:
     // access the processor object that created it.
     SimpleMBCompAudioProcessor& audioProcessor;
 
-    Placeholder controlBar, analyzer, globalControls, bandControls;
+    Placeholder controlBar, analyzer, /*globalControls,*/ bandControls;
+    GlobalControls globalControls{ audioProcessor.apvts }; //learn more about the apvts
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleMBCompAudioProcessorEditor)
 };
