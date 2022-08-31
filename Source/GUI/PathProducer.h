b4430af6890f68a3523864d3216cf0c92a553aa5
Kyle Whaley <kitikdub@gmail.com>
2022-08-31 18:43:39 -0400

    Refactor for Spectrum Analyzer

---
+++ Source/GUI/PathProducer.h
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    PathProducer.h
+    Created: 31 Aug 2022 5:30:45pm
+    Author:  kylew
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+#include "FFTDataGenerator.h"
+#include "AnalyzerPathGenerator.h"
+#include "../PluginProcessor.h"
+
+struct PathProducer
+{
+    PathProducer(SingleChannelSampleFifo<SimpleMBCompAudioProcessor::BlockType>& scsf) :
+        leftChannelFifo(&scsf)
+    {
+        leftChannelFFTDataGenerator.changeOrder(FFTOrder::order2048);
+        monoBuffer.setSize(1, leftChannelFFTDataGenerator.getFFTSize());
+    }
+    void process(juce::Rectangle<float> fftBounds, double sampleRate);
+    juce::Path getPath() { return leftChannelFFTPath; }
+private:
+    SingleChannelSampleFifo<SimpleMBCompAudioProcessor::BlockType>* leftChannelFifo;
+
+    juce::AudioBuffer<float> monoBuffer;
+
+    FFTDataGenerator<std::vector<float>> leftChannelFFTDataGenerator;
+
+    AnalyzerPathGenerator<juce::Path> pathProducer;
+
+    juce::Path leftChannelFFTPath;
+};
