17947e4ace961ad612e089cccd0a4cb18448c188
Kyle Whaley <kitikdub@gmail.com>
2022-08-31 18:48:06 -0400

    Hooked up Spectrum Analyzer

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -186,6 +186,9 @@ void SimpleMBCompAudioProcessor::prepareToPlay(double sampleRate, int samplesPer
     {
         buffer.setSize(spec.numChannels, samplesPerBlock);
     }
+
+    leftChannelFifo.prepare(samplesPerBlock);
+    rightChannelFifo.prepare(samplesPerBlock);
 }
 
 void SimpleMBCompAudioProcessor::releaseResources()
@@ -279,6 +282,9 @@ void SimpleMBCompAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, 
         buffer.clear(i, 0, buffer.getNumSamples());
 
     updateState();
+
+    leftChannelFifo.update(buffer);
+    rightChannelFifo.update(buffer);
 
     applyGain(buffer, inputGain);
 
