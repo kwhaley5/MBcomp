4f2d85ac6301594c8c7c90529a9326de8e7db7c0
Kyle Whaley <kitikdub@gmail.com>
2022-09-01 18:33:44 -0400

    Fixed neg infinity line being visible in spectrum Analyzer
    Refactered FFT paths as well

--- Source/GUI/SpectrumAnalyzer.cpp
+++ Source/GUI/SpectrumAnalyzer.cpp
@@ -36,6 +36,28 @@ SpectrumAnalyzer::~SpectrumAnalyzer()
     }
 }
 
+void SpectrumAnalyzer::drawFFTAnalysis(juce::Graphics& g, juce::Rectangle<int> bounds)
+{
+    using namespace juce;
+
+    auto responseArea = getAnalysisArea(bounds);
+
+    Graphics::ScopedSaveState sss(g);
+    g.reduceClipRegion(responseArea);
+
+    auto leftChannelFFTPath = leftPathProducer.getPath();
+    leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), 0));
+
+    g.setColour(Colour(97u, 18u, 167u)); //purple-
+    g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
+
+    auto rightChannelFFTPath = rightPathProducer.getPath();
+    rightChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), 0));
+
+    g.setColour(Colour(215u, 201u, 134u));
+    g.strokePath(rightChannelFFTPath, PathStrokeType(1.f));
+}
+
 void SpectrumAnalyzer::paint(juce::Graphics& g)
 {
     using namespace juce;
@@ -46,21 +68,9 @@ void SpectrumAnalyzer::paint(juce::Graphics& g)
 
     drawBackgroundGrid(g, bounds);
 
-    auto responseArea = getAnalysisArea(bounds);
-
     if (shouldShowFFTAnalysis)
     {
-        auto leftChannelFFTPath = leftPathProducer.getPath();
-        leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), 0));
-
-        g.setColour(Colour(97u, 18u, 167u)); //purple-
-        g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
-
-        auto rightChannelFFTPath = rightPathProducer.getPath();
-        rightChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), 0));
-
-        g.setColour(Colour(215u, 201u, 134u));
-        g.strokePath(rightChannelFFTPath, PathStrokeType(1.f));
+        drawFFTAnalysis(g, bounds);
     }
 
     Path border;
